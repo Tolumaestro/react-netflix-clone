9d83467e3549bc443e82901d30cb3cac483b0e51
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-09-13 12:28:06 +0100

    initialized react app and implemented redux

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA1bPywlsk_Uph3cFaOfbWBA1GCtjTla2k",
+  authDomain: "netflix-clone-e98ee.firebaseapp.com",
+  projectId: "netflix-clone-e98ee",
+  storageBucket: "netflix-clone-e98ee.appspot.com",
+  messagingSenderId: "856510714699",
+  appId: "1:856510714699:web:d829bf23136f0d97620ece",
+  measurementId: "G-4YDP7FC6D5",
+};
